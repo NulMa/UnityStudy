ef201fa8b085ab788dcb9d74b61badf143eb7c46
NullMa <jungwt524@gmail.com>
2025-11-24 17:19:11 +0900

    25_11_24_1718

---
+++ StarTree/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace StarTree {
+    internal class Program {
+        static void Main(string[] args) {
+            tree();
+            Console.WriteLine();
+            reverseTree();
+            Console.WriteLine();
+            piramid();
+            Console.WriteLine();
+            reversePiramid();
+
+            void tree() {
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j <= i; j++) {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            void reverseTree() {
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j < 5 - i; j++) {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
