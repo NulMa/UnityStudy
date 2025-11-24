49432f5c7b97f44952cec9e99c556218705e9586
NullMa <jungwt524@gmail.com>
2025-11-24 16:33:26 +0900

    25_11_24_1633

--- 25_11_24/Program.cs
+++ 25_11_24/Program.cs
@@ -1,12 +1,7 @@
 ﻿namespace _25_11_24 {
     internal class Program {
         static void Main(string[] args) {
-            int num = 1;
-
-            while (num < 11) {
-                Console.WriteLine(num);
-                num++;
-            }
+            
         }
     }
 }
---
+++ multiplication_table/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace multiplication_table {
+    internal class Program {
+        static void Main(string[] args) {
+            for(int row = 2; row < 10; row++) {
+                Console.WriteLine( row + "단 :\n");
+                for(int col = 1; col < 10; col++) {
+                    Console.WriteLine(row + " * " + col + " : " + col*row);
+                }
+                Console.WriteLine("\n\n");
+            }
+        }
+    }
+}
