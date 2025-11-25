4196dca154526891209af2f6eaf6fb2e0c12a100
NullMa <jungwt524@gmail.com>
2025-11-25 09:42:22 +0900

    CumulativeSum_Fix

--- Cumulative sum/Program.cs
+++ Cumulative sum/Program.cs
@@ -1,15 +1,17 @@
 ﻿namespace Cumulative_sum {
     internal class Program {
         static void Main(string[] args) {
-            int input = 10;
+            Console.Write("Input Your Value : ");
+            string userInput = Console.ReadLine();
 
+            int input = 10;
             int half = input / 2;
-            int remain = input % 2;
             int answer = 0;
 
             for(int i = 0; i < half; i++) {
                 answer += (1 + input);
             }
+
             Console.WriteLine("Cumulative Sum is : " + answer);
         }
     }
