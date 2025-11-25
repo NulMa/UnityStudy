8173d9a5df5a5f9c99ce3768577e07f8dd4f19aa
NullMa <jungwt524@gmail.com>
2025-11-25 09:26:52 +0900

    25_11_25_0926

---
+++ Cumulative sum/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Cumulative_sum {
+    internal class Program {
+        static void Main(string[] args) {
+            int input = 10;
+
+            int half = input / 2;
+            int remain = input % 2;
+            int answer = 0;
+
+            for(int i = 0; i < half; i++) {
+                answer += (1 + input);
+            }
+            Console.WriteLine("Cumulative Sum is : " + answer);
+        }
+    }
+}
