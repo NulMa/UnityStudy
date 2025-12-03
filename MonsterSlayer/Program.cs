59288f48f40c79e8e0c8195cac7d181e275c91b3
NullMa <jungwt524@gmail.com>
2025-12-03 22:18:42 +0900

    fix : Change_indentation_to_Allman

--- MonsterSlayer/Program.cs
+++ MonsterSlayer/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System.Security.Cryptography.X509Certificates;
 using static System.Net.Mime.MediaTypeNames;
 
-namespace MonsterSlayer {
+namespace MonsterSlayer
+{
 
-    class Character {
+    class Character
+    {
         public string Name;
         public int Hp;
         public int Atk;
@@ -11,9 +13,7 @@ namespace MonsterSlayer {
 
         public Character(string name, int hp, int atk) {    }
 
-        public virtual void Attack(Character target) {
-
-        }
+        public virtual void Attack(Character target){ }
 
         public bool IsDead() {
             if (Hp <= 0)
@@ -22,27 +22,32 @@ namespace MonsterSlayer {
         }
     }
 
-    class Player : Character {
-
+    class Player : Character
+    {
         public int Exp = 0;
 
-        public Player(string name, int hp, int atk) : base(name, hp, atk) {
+        public Player(string name, int hp, int atk) : base(name, hp, atk)
+        {
             Name = name;
             Hp = hp;
             Atk = atk;
         }
 
-        public void GainExp(int amount) {
+        public void GainExp(int amount)
+        {
             Exp += amount;
         }
 
-        public override void Attack(Character target) {
+        public override void Attack(Character target)
+        {
             Random rand = new Random();
             int damage = rand.Next(Atk - 5, Atk + 15);
             target.Hp -= damage;
 
             Thread.Sleep(100);
-            if (damage > 20) {
+
+            if (damage > 20)
+            {
                 Console.ForegroundColor = ConsoleColor.Magenta;
                 Console.WriteLine("\n     <<Critical Hit!>>");
                 Console.ResetColor();
@@ -52,18 +57,22 @@ namespace MonsterSlayer {
         }
     }
 
-    class Monster : Character {
-        public Monster(string name) : base(name, 0, 0) {
+    class Monster : Character
+    {
+        public Monster(string name) : base("", 0, 0)
+        {
             Name = RandomName();
             Random rand = new Random();
             Hp = rand.Next(20, 51);
             Atk = rand.Next(2, 7);
         }
-        static string RandomName() {
+        static string RandomName()
+        {
             string[] names = { "Slime", "Goblin", "Wolf", "Bat" };
             return names[new Random().Next(names.Length)];
         }
-        public override void Attack(Character target) {
+        public override void Attack(Character target)
+        {
             Random rand = new Random();
             target.Hp -= rand.Next(Atk - 5, Atk + 2);
             Thread.Sleep(100);
@@ -74,31 +83,40 @@ namespace MonsterSlayer {
 
 
 
-    internal class Program {
-        public static bool AskToContinue() {
+    internal class Program
+    {
+        public static bool AskToContinue()
+        {
             Console.WriteLine("Do you want to continue? (y/n)");
             string input = Console.ReadLine();
-            if (input.ToLower() == "y") {
+            if (input.ToLower() == "y")
+            {
                 return true;
             }
-            else {
+            else
+            {
                 return false;
             }
         }
 
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
             Random rand = new Random();
             Player player = new Player("Player", 100, 10);
-
             bool gameRunning = true;
 
-            while (gameRunning) {
+            while (gameRunning)
+            {
                 Monster monster = new Monster("");
                 Console.WriteLine($"\n\nYou Found {monster.Name}!\n (HP: {monster.Hp}, ATK: {monster.Atk})");
                 Thread.Sleep(300);
-                while (!monster.IsDead()) {
+
+                while (!monster.IsDead())
+                {
                     player.Attack(monster);
-                    if (monster.IsDead()) {
+
+                    if (monster.IsDead())
+                    {
                         Console.WriteLine($"You Slay {monster.Name}.");
                         player.GainExp(10);
                         Console.WriteLine($"gain 10 EXP! Current EXP: {player.Exp}");
@@ -107,13 +125,15 @@ namespace MonsterSlayer {
                         Thread.Sleep(1000);
                         break;
                     }
+
                     monster.Attack(player);
-                    if (player.IsDead()) {
+
+                    if (player.IsDead())
+                    {
                         Console.WriteLine("YOU DIED.");
                         return;
                     }
                 }
-
             }
         }
     }
