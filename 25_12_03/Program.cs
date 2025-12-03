6c856a582c1b9ac98eaac326d07c41ed58856996
NullMa <jungwt524@gmail.com>
2025-12-03 11:00:05 +0900

    feat : 251203_1059_CCS_Recursive

--- 25_12_03/Program.cs
+++ 25_12_03/Program.cs
@@ -1,15 +1,39 @@
 ﻿namespace _25_12_03 {
     internal class Program {
-        static void Swap(ref int a,ref int b) {
+
+        static void Main() {
+            int result = cumculativeSum(1, 6);
+            Console.WriteLine(result);
+        }
+        static void Swap(out int a, out int b) {
+            a = 0;
+            b = 0;
             int temp = a;
             a = b;
             b = temp;
         }
-        static void Main() {
-            int x = 10;
-            int y = 20;
-            Swap(ref x, ref y);
-            Console.WriteLine($"{x}, {y}")
+
+        static int cumculativeSum(int strt, int nd) {
+            if (strt > nd)
+                return 0;
+
+            return strt + cumculativeSum(strt + 1, nd);
+        }
+
+        // optional parameter   
+        static void Add(int a, int b, int c = 0, int d = 0, int e = 1) {
+
+        }
+
+        
+
+        // overloading
+        static void Add(int a) {
+            a++;
+        }
+
+        static void Add(int a, int b) {
+            int temp = a+b;
         }
     }
 }
