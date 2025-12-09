59eb52de5be01054dea65d3bce146fcc18cf4030
NullMa <jungwt524@gmail.com>
2025-12-09 16:18:03 +0900

    feat : bj_10808

---
+++ 251209CountingStars/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _251209CountingStars {
+    internal class Program
+    {
+        static void Main(string[] args) {
+            int[] count = new int[26];
+
+            string str = Console.ReadLine();
+            Console.WriteLine(str);
+
+            foreach (char c in str) {
+                for(int i = 0; i < 27; i++) {
+                    if ((int)c == 97+i)
+                        count[i]++;
+                }
+            }
+            foreach (int i in count) {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
